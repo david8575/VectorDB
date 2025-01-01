70ac45b301dac186700ebd0d72667549ea67e9fa
parksehun <davidpark8575@gmail.com>
2025-01-01 17:02:41 +0900

    simpsons text updated

---
+++ TextEmbedding/simpsons_dialogue.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from collections import Counter
+df = pd.read_csv("datasets/simpsons_dataset.csv")
+print(df.shape)
+counts = Counter(df["raw_character_text"])
+print(counts.most_common())
